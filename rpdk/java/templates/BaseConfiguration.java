4cb588eceb31ef7037ebc5435c561310d7c26028
Toby Fleming <tobflem@amazon.com>
2019-02-06 11:10:36 -0800

    Move Java plugin to Java repo (#28)

---
+++ rpdk/java/templates/BaseConfiguration.java
@@ -0,0 +1,13 @@
+// This is a generated file. Modifications will be overwritten.
+package {{ package_name }};
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public abstract class BaseConfiguration {
+
+    protected final String schemaFilename;
+
+}
