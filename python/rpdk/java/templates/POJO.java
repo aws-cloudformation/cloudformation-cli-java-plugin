99bc695456b51e8916d6808ee485d18f74aab19a
Aiden Yang <yngfn@amazon.com>
2019-05-16 13:40:34 -0700

    Add Import For @AllArgsConstructor

--- python/rpdk/java/templates/POJO.java
+++ python/rpdk/java/templates/POJO.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Map;
 import java.util.List;
 import java.util.Set;
+import lombok.AllArgsConstructor;
 import lombok.Setter;
 
 @Setter
