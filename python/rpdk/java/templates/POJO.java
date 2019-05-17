b7633fe547bddcbed06219d75d57cbf11ccd6db4
Aiden Yang <yngfn@amazon.com>
2019-05-17 15:35:56 -0700

    Add @Builder For POJO template

--- python/rpdk/java/templates/POJO.java
+++ python/rpdk/java/templates/POJO.java
@@ -6,10 +6,12 @@ import java.util.Map;
 import java.util.List;
 import java.util.Set;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class {{ pojo_name|uppercase_first_letter }} {
