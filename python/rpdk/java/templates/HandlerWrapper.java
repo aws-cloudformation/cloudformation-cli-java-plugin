75b8582fe547026961fd397d3be8cf61e1f3fea4
Ryan Lohan <ryanloha@amazon.com>
2019-05-31 10:51:28 -0700

    Passes logical resource identifier (name)
    The logicalResourceIdentifier from the caller (such as the name of a resource in a CloudFormation template) is now passed to resource handlers to enable them to be used in name/identity auto-generation or emission of logs and diagnostics.

--- python/rpdk/java/templates/HandlerWrapper.java
+++ python/rpdk/java/templates/HandlerWrapper.java
@@ -83,6 +83,7 @@ public final class HandlerWrapper extends LambdaWrapper<{{ pojo_name }}, Callbac
         return new ResourceHandlerRequest<>(
             request.getAwsAccountId(),
             request.getBearerToken(),
+            request.getLogicalResourceIdentifier(),
             request.getNextToken(),
             request.getRegion(),
             request.getResourceType(),
