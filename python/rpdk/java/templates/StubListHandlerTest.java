964ae48d605ff13fb165771c6f5ff27e17b1d553
Ryan Lohan <ryanloha@amazon.com>
2019-06-10 09:33:57 -0700

    Added Mockito extension

--- python/rpdk/java/templates/StubListHandlerTest.java
+++ python/rpdk/java/templates/StubListHandlerTest.java
@@ -7,11 +7,14 @@ import com.amazonaws.cloudformation.proxy.ProgressEvent;
 import com.amazonaws.cloudformation.proxy.ResourceHandlerRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
+@ExtendWith(MockitoExtension.class)
 public class {{ operation }}HandlerTest {
 
     @Mock
