cbe1f5ea82d59afd979ce759187480a8818dae04
dchakrav-github <42748809+dchakrav-github@users.noreply.github.com>
2019-07-08 14:46:02 -0700

    Moved Delay to use java.time.Duration (#123)
    * * Moved Delay to use java.time.Duration to specify timeout and delay in
    seconds, minutes or days
    * Fix imports, checkstyle errors, Increase coverage
    
    * Fix space issues
    
    * Fix as done my mvn spotless:apply

---
+++ src/main/java/com/amazonaws/cloudformation/proxy/delay/Blended.java
@@ -0,0 +1,83 @@
+/*
+* Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+package com.amazonaws.cloudformation.proxy.delay;
+
+import com.amazonaws.cloudformation.proxy.Delay;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Provides blended delay of seconds for each attempt until all delays in the
+ * order start to return {@link Duration#ZERO}. This is useful to model blends
+ * in the delays where on can be quick for the first set of delays using
+ * {@link Constant} and then become {@link MultipleOf} or {@link Exponential}
+ * thereafter.
+ *
+ * {@code
+ *
+ *     final Delay delay = Blended.of()
+ *        .add(
+ *            Constant.of().delay(Duration.ofSeconds(5))
+ *                .timeout(Duration.ofSeconds(20)).build())
+ *        .add(ShiftByMultipleOf.shiftedOf()
+ *            .delay(Duration.ofSeconds(5)).timeout(Duration.ofSeconds(220))
+ *            .build())
+ *        .build();
+ * }
+ *
+ * The above delay provides the following set of 5, 10, 15, 20, 40, 90, 150, 220
+ */
+public class Blended implements Delay {
+
+    private int index;
+    private final List<Delay> inOrder;
+
+    private Blended(List<Delay> delays) {
+        index = 0;
+        inOrder = delays;
+    }
+
+    public static BlendedBuilder of() {
+        return new BlendedBuilder();
+    }
+
+    public static final class BlendedBuilder implements Builder<Blended> {
+        private final List<Delay> inOrder = new ArrayList<>();
+
+        public BlendedBuilder add(Delay delay) {
+            inOrder.add(delay);
+            return this;
+        }
+
+        public Blended build() {
+            return new Blended(inOrder);
+        }
+    }
+
+    @Override
+    public Duration nextDelay(int attempt) {
+        Duration next = Duration.ZERO;
+        while (index < inOrder.size()) {
+            next = inOrder.get(index).nextDelay(attempt);
+            if (next != Duration.ZERO) {
+                break;
+            }
+            ++index;
+        }
+        return next;
+    }
+}
---
+++ src/main/java/com/amazonaws/cloudformation/proxy/delay/MinDelayAbstractBase.java
@@ -0,0 +1,45 @@
+/*
+* Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+package com.amazonaws.cloudformation.proxy.delay;
+
+import com.google.common.base.Preconditions;
+
+import java.time.Duration;
+
+/**
+ * Base class for all [min, timeout) range based
+ * {@link com.amazonaws.cloudformation.proxy.Delay}s
+ *
+ * @see Exponential
+ */
+abstract class MinDelayAbstractBase extends AbstractDelay {
+    final Duration minDelay;
+
+    MinDelayAbstractBase(Duration timeout,
+                         Duration minDelay) {
+        super(timeout);
+        Preconditions.checkArgument(minDelay != null && minDelay.toMillis() >= 0, "minDelay must be > 0");
+        Preconditions.checkArgument(minDelay.compareTo(timeout) < 0, "minDelay < timeout");
+        this.minDelay = minDelay;
+    }
+
+    @Override
+    protected Duration enforceBounds(Duration boundsCheck, Duration delayTime) {
+        if (boundsCheck.compareTo(minDelay) < 0) {
+            return minDelay;
+        }
+        return super.enforceBounds(boundsCheck, delayTime);
+    }
+}
