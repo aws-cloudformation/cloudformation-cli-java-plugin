cefa252bc628a896b2611cb22740f450f57a9bbc
John Tompkins <jotompki@amazon.com>
2020-02-07 09:35:51 -0800

    Support protocol version 2.0.0 to callback handlers orchestrated by cloudformation service

--- src/main/java/software/amazon/cloudformation/proxy/RequestData.java
+++ src/main/java/software/amazon/cloudformation/proxy/RequestData.java
@@ -22,7 +22,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RequestData<ResourceT> {
     private Credentials callerCredentials;
-    private Credentials platformCredentials;
     private Credentials providerCredentials;
     private String providerLogGroupName;
     private String logicalResourceId;
