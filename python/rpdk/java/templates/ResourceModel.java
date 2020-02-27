96616def5699572c42798cf42ddfb15eb99cf144
xiwhuang <xiwhuang@amazon.com>
2020-02-27 15:09:20 -0800

    Bug fix - Unexpected properties duplicates when validating model (#235)

--- python/rpdk/java/templates/ResourceModel.java
+++ python/rpdk/java/templates/ResourceModel.java
@@ -1,6 +1,8 @@
 // This is a generated file. Modifications will be overwritten.
 package {{ package_name }};
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Map;
@@ -18,6 +20,7 @@ import org.json.JSONObject;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
 public class {{ model_name|uppercase_first_letter }} {
     @JsonIgnore
     public static final String TYPE_NAME = "{{ type_name }}";
