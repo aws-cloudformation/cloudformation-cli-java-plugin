1abf3f08f3a0b843db0db122048f9a2bd11849a1
Maria Ines Parnisari <mparnisa@amazon.com>
2020-06-08 11:59:53 -0700

    Add tear down method to handler tests (#262)

--- python/rpdk/java/templates/init/guided_aws/StubHandlerTest.java
+++ python/rpdk/java/templates/init/guided_aws/StubHandlerTest.java
@@ -7,6 +7,7 @@ import software.amazon.cloudformation.proxy.OperationStatus;
 import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.ProxyClient;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +16,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 @ExtendWith(MockitoExtension.class)
 public class {{ operation }}HandlerTest extends AbstractTestBase {
@@ -33,6 +37,12 @@ public class {{ operation }}HandlerTest extends AbstractTestBase {
         proxy = new AmazonWebServicesClientProxy(logger, MOCK_CREDENTIALS, () -> Duration.ofSeconds(600).toMillis());
         sdkClient = mock(SdkClient.class);
         proxyClient = MOCK_PROXY(proxy, sdkClient);
+    }
+
+    @AfterEach
+    public void tear_down() {
+        verify(sdkClient, atLeastOnce()).serviceName();
+        verifyNoMoreInteractions(sdkClient);
     }
 
     @Test
