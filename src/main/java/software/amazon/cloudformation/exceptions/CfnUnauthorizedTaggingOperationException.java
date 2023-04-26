6dea2e4542326b10586255179b2093e188653a80
Anton Mokhovikov <ammokhov@amazon.com>
2023-04-25 17:03:04 -0700

    Tagging Specific Error Code

---
+++ src/main/java/software/amazon/cloudformation/exceptions/CfnUnauthorizedTaggingOperationException.java
@@ -0,0 +1,31 @@
+/*
+* Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+package software.amazon.cloudformation.exceptions;
+
+import software.amazon.cloudformation.proxy.HandlerErrorCode;
+
+public class CfnUnauthorizedTaggingOperationException extends BaseHandlerException {
+
+    private static final long serialVersionUID = -1646136434112354328L;
+    private static final HandlerErrorCode ERROR_CODE = HandlerErrorCode.UnauthorizedTaggingOperation;
+
+    public CfnUnauthorizedTaggingOperationException() {
+        this(null);
+    }
+
+    public CfnUnauthorizedTaggingOperationException(final Throwable cause) {
+        super(ERROR_CODE.getMessage(), cause, ERROR_CODE);
+    }
+}
--- src/main/java/software/amazon/cloudformation/proxy/delay/CappedExponential.java
+++ src/main/java/software/amazon/cloudformation/proxy/delay/CappedExponential.java
@@ -1,7 +1,20 @@
+/*
+* Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
 package software.amazon.cloudformation.proxy.delay;
 
 import com.google.common.base.Preconditions;
-
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
@@ -13,7 +26,10 @@ public class CappedExponential extends MinDelayAbstractBase {
 
     final Duration maxDelay;
 
-    CappedExponential(Duration timeout, Duration minDelay, Double powerBy, Duration maxDelay) {
+    CappedExponential(Duration timeout,
+                      Duration minDelay,
+                      Double powerBy,
+                      Duration maxDelay) {
         super(timeout, minDelay);
         Preconditions.checkArgument(powerBy >= 1.0, "powerBy >= 1.0");
         Preconditions.checkArgument(maxDelay != null && maxDelay.toMillis() >= 0, "maxDelay must be > 0");
@@ -29,7 +45,7 @@ public class CappedExponential extends MinDelayAbstractBase {
 
     public static final class Builder extends MinDelayBasedBuilder<CappedExponential, Builder> {
         private double powerBy = 2;
-        private Duration maxDelay =Duration.ofSeconds(20);
+        private Duration maxDelay = Duration.ofSeconds(20);
 
         public CappedExponential.Builder powerBy(Double powerBy) {
             this.powerBy = powerBy;
